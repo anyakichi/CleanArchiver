459d92bac6bfac97cf4419d0ce2c8420fc0883f0
Anyakichi <anyakichi@sopht.jp>
2009-11-07 14:28:57 +0000

    Convert Unicode normalization form transparently on Mac OS X. Suggested by ChangBeom Park.
    --HG--
    extra : convert_revision : svn%3A8e53915d-3802-0410-9d24-df610d03f11b/trunk%4084

--- p7zip/CPP/Common/StringConvert.cpp
+++ p7zip/CPP/Common/StringConvert.cpp
@@ -17,13 +17,59 @@ namespace utf8
 
 #ifdef LOCALE_IS_UTF8
 
+#ifdef ENV_MACOSX
+namespace mac
+{
+#include <CoreFoundation/CFString.h>
+}
+#endif
+
 UString MultiByteToUnicodeString(const AString &srcString, UINT codePage)
 {
   if ((global_use_utf16_conversion) && (!srcString.IsEmpty()))
   {
+#ifdef ENV_MACOSX
+    UString resultString;
+    mac::CFStringRef string;
+    mac::CFMutableStringRef mutableString;
+    mac::UniChar *uString;
+    int len;
+    const char *src = srcString;
+
+    string = mac::CFStringCreateWithBytes(NULL, (const mac::UInt8 *)src,
+					  strlen(src),
+					  mac::kCFStringEncodingUTF8, false);
+    if (string == NULL)
+      goto fail1;
+    mutableString = mac::CFStringCreateMutableCopy(NULL, 0, string);
+    if (mutableString == NULL)
+      goto fail2;
+    mac::CFStringNormalize(mutableString, mac::kCFStringNormalizationFormC);
+
+    len = mac::CFStringGetLength(mutableString);
+    uString = (mac::UniChar *)malloc(len * sizeof(mac::UniChar));
+    if (uString == NULL)
+      goto fail3;
+    mac::CFStringGetCharacters(mutableString, mac::CFRangeMake(0, len),
+			       uString);
+
+    for (int i = 0; i < len; i++)
+      resultString += wchar_t(uString[i]);
+
+    free(uString);
+fail3:
+    CFRelease(mutableString);
+fail2:
+    CFRelease(string);
+fail1:
+
+    if (!resultString.IsEmpty())
+      return resultString;
+#else /* ENV_MACOSX */
     UString resultString;
     bool bret = utf8::ConvertUTF8ToUnicode(srcString,resultString);
     if (bret) return resultString;
+#endif /* ENV_MACOSX */
   }
 
   UString resultString;
@@ -37,9 +83,49 @@ AString UnicodeStringToMultiByte(const UString &srcString, UINT codePage)
 {
   if ((global_use_utf16_conversion) && (!srcString.IsEmpty()))
   {
+#ifdef ENV_MACOSX
+    AString resultString;
+    mac::CFStringRef string;
+    mac::CFMutableStringRef mutableString;
+    mac::UniChar *uString;
+    const wchar_t *wstring;
+    char *p;
+    int len, max;
+
+    wstring = srcString;
+    len = srcString.Length();
+    uString = (mac::UniChar *)malloc(len * sizeof(mac::UniChar));
+    for (int i = 0; i < len; i++)
+      uString[i] = wstring[i];
+    string = mac::CFStringCreateWithCharacters(NULL, uString, len);
+    if (string == NULL)
+      goto fail1;
+
+    mutableString = mac::CFStringCreateMutableCopy(NULL, 0, string);
+    if (mutableString == NULL)
+      goto fail2;
+    mac::CFStringNormalize(mutableString, mac::kCFStringNormalizationFormD);
+
+    max = mac::CFStringGetMaximumSizeForEncoding(len,
+						 mac::kCFStringEncodingUTF8);
+
+    p = resultString.GetBuffer(max + 1);
+    mac::CFStringGetCString(mutableString, p, max + 1,
+			    mac::kCFStringEncodingUTF8);
+    resultString.ReleaseBuffer();
+
+    mac::CFRelease(mutableString);
+fail2:
+    mac::CFRelease(string);
+fail1:
+
+    if (!resultString.IsEmpty())
+      return resultString;
+#else /* ENV_MACOSX */
     AString resultString;
     bool bret = utf8::ConvertUnicodeToUTF8(srcString,resultString);
     if (bret) return resultString;
+#endif /* ENV_MACOSX */
   }
 
   AString resultString;
